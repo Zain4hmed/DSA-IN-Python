6abe97b65cabb9487a115b041e157bce359c4cb0
zain ahmed <zainahmed01997@gmail.com>
2023-09-03 22:52:40 +0530

    completed ll including the reversal of singly linked list

--- LinkedList_Implimentation.py
+++ LinkedList_Implimentation.py
@@ -122,7 +122,34 @@ class LinkedList:
         next_node = temp
         previous_node.next = new_node
         new_node.next = next_node
+        self.length += 1
         return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
+        prev = self.get(index-1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
 
 
 myLinkedList = LinkedList(1)
@@ -131,12 +158,5 @@ myLinkedList.append(2)
 myLinkedList.append(3)
 myLinkedList.append(4)
 
-
-print(myLinkedList.get(4).value)
-print(myLinkedList.set_value(4, 3))
-print(myLinkedList.get(4).value)
-
-myLinkedList.insert(4, 4)
-
-print("printing the linked list")
+myLinkedList.reverse()
 myLinkedList.print_list()
